df27f7bffec57d0bd723ff02515b7fcc541789dc
osaiebiz <keerthivasan@osaiebiz.com>
2024-07-17 22:45:37 +0530

    first commit

--- index.js
+++ index.js
@@ -5,19 +5,7 @@ import routes from "./routes/router.js";
 
 dotenv.config()
 const app = express()
-const corsOptions = {
-    origin: function (origin, callback) {
-        const allowedOrigins = ["https://novactech-helpdesk.freshservice.com"];
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    optionsSuccessStatus: 200
-};
-
-app.use(cors(corsOptions));
+app.use(cors({ origin: "*" }))
 app.use(express.json())
 app.use("/", routes)
 app.listen(process.env.PORT, () => {
